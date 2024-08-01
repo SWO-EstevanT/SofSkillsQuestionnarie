c0860c2410b91e88ec8a81bc4389bb45667a5fba
SWO-EstevanT <estevan.tangarife@softwareone.com>
2024-08-01 12:13:05 -0500

    Save chances
    save some files while restart machine

---
+++ User.Api/Controllers/UserController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MicroserviceUser.Domain.Entities.User;
+namespace User.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+
+
+        [HttpGet]
+        public async Task<List<User>> getUsers() { 
+        
+
+        
+        
+        }
+
+
+
+
+
+
+
+
+    }
+}
