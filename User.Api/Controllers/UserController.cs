7660bca4254c51d98049acd5c5f30c78bd342dbb
SWO-EstevanT <estevan.tangarife@softwareone.com>
2024-08-02 09:14:33 -0500

    Chances

--- User.Api/Controllers/UserController.cs
+++ User.Api/Controllers/UserController.cs
@@ -1,25 +1,27 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
-using MicroserviceUser.Domain.Entities.User;
-namespace User.Api.Controllers
+
+
+
+
+namespace Users.Api.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     [ApiController]
     public class UserController : ControllerBase
     {
+        //private readonly iuserusecase _userusecase;
+        //private readonly imapper _mapper;
 
 
         [HttpGet]
-        public async Task<List<User>> getUsers() { 
-        
-
-        
-        
+        public async Task<List<User>> GetUsersAsync()
+        {
+            //var users = await _userusecase.getusersasync();
+            //return _mapper.map<List<User>>(users);
+            return null;
         }
-
-
-
-
 
 
 
--- User.cs
+++ User.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using system;
+
+namespace MicroserviceUser.Domain.Entities;
 
 public class User
 {
