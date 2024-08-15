31b8519f5819e68d09a38292355230dfe91e35aa
SWO-EstevanT <estevan.tangarife@softwareone.com>
2024-08-15 11:50:11 -0500

    Entities
    In that case i´m created some entities for the proyect

---
+++ MicroserviceUser.Domain/Entities/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroserviceUser.Domain.Entities
+{
+    public class User
+    {
+        public string Id { get; set; }
+        public string uidUser { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public int role { get; set; }
+        //public int stateUser { get; set; }
+        
+
+    }
+}
