b4edeb9109a44c2bb7ade6cd0c88186c11ba6164
SWO-EstevanT <estevan.tangarife@softwareone.com>
2024-08-22 11:25:35 -0500

    New entity
    in this case im created a new entity for the proyect

---
+++ MicroserviceUser.Domain/Commands/CreateSurvey.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroserviceUser.Domain.Commands
+{
+    public class CreateSurvey
+    {
+
+        public string tittle { get; set; }
+        public DateTime? dateCreation { get; set; }
+
+        //public string enginnerId { get; set; }
+        public int state { get; set; }
+
+
+    }
+}
